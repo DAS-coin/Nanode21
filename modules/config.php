c3f4cea22ccb3f6c19c3e9394a7d7c1f74bb211e
stefonarch <stef.binde@riseup.net>
2018-03-08 07:51:07 +0100

    Update config.php

--- modules/config.php
+++ modules/config.php
@@ -11,6 +11,13 @@ $cmcTickerUrl = 'https://api.coinmarketcap.com/v1/ticker/?convert=EUR';
 // version number
 $versionString = '1.1';
 
+//------------- Custom ----------------
+
+$siteTitle ='Nano node Monitor'    //title of the webpage
+$serverInfo ='my linux server'
+$monthlyCosts = '$5'
+$nanoDonationAccount = 'xrb_1trxfmdiq5s1qaihmrj3ef5kscht8ymocosnze6nrizru5ki1dbaqt7w3z1k';
+
 // ----------- Nano Node Variables -----------
 
 // ip address for RPC (default: 127.0.0.1)
@@ -22,8 +29,3 @@ $nanoNodeRPCPort = '7076';
 // node account
 $nanoNodeAccount = 'xrb_1i9ugg14c5sph67z4st9xk8xatz59xntofqpbagaihctg6ngog1f45mwoa54'; 
 
-//------------- Misc ----------------
-$serverInfo ='my linux server'
-$siteTitle ='Nano node Monitor'    //title of the webpage
-// donation account
-$nanoDonationAccount = 'xrb_1trxfmdiq5s1qaihmrj3ef5kscht8ymocosnze6nrizru5ki1dbaqt7w3z1k';
